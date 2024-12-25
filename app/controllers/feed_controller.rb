f569c561331a9188c215038e5d5ff16a813d02e8
Doug Turnbull <doug@dahlialabs.ai>
2024-12-25 18:11:29 -0500

    Add more tweets generation

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,8 +1,12 @@
 class FeedController < ApplicationController
   def index
-    @posts = [
-      { content: "Hello, world!" },
-      { content: "Goodbye, world!" }
-    ]
+    @offset = params[:offset] || 10
+    @posts = []
+    puts "Offset: #{@offset}"
+    @offset.to_i.times do |i|
+      @posts.push({ user: "user#{i}", content: "This is post number #{i}",
+                    scroll_posn: i })
+    end
+  @next_offset = @offset.to_i + 10
   end
 end
